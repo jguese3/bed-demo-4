a019fa11342c725b035914bb0e924528901dd4a7
Jarone Guese <jguese3@academic.rrc.ca>
2025-10-06 12:42:04 -0500

    Feat: Implemented AppError class - Base error class for all application error.

--- src/api/v1/errors/errors.ts
+++ src/api/v1/errors/errors.ts
@@ -1,11 +1,28 @@
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 
+/**
+ * Base error class for all application error
+ * Extends the built-in Error class to include an error code and status code
+ * 
+ * This abstract class provides:
+ * - A consistent structure accross our app
+ * - HTTP status codes for an API responses
+ */
 export class AppError extends Error {
+    /**
+     * Creates a new AppError instance
+     * @param message The error message
+     * @param code The error code
+     * @param statusCode An http response code
+     */
     constructor(
         public message: string,
         public code: string,
         public statusCode: number
     ){
-        super()
+        super(message);
+        this.name = this.constructor.name;
+        Object.setPrototypeOf(this, new.target.prototype);
+        Error.captureStackTrace(this, this.constructor);
     }
 }
